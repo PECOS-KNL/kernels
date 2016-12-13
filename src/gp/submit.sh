29c81e6c84a8e4a45c31954de07717ad8f9f643f
Damon McDougall <damon@ices.utexas.edu>
2016-12-13 13:55:07 -0600

    Update submission script

--- src/gp/submit.sh
+++ src/gp/submit.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH -J gp
 #SBATCH -o gp_%j.txt
-#SBATCH -n 1
-#SBATCH -p development
-#SBATCH -t 01:00:00
+#SBATCH -n 68
+#SBATCH -N 1
+#SBATCH -p normal
+#SBATCH -t 02:00:00
 #SBATCH --mail-user=damon@ices.utexas.edu
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 env
 echo -n "Begin: "
 date
-./gp 288 4096
+ibrun -np 1 ./gp 256 queso_input.txt 40000
 echo -n "End: "
 date
