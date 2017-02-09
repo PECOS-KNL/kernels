8cbd1342058012ae5f2ef770e5998bca7f42cfdc
Damon McDougall <damon@ices.utexas.edu>
2017-02-09 14:11:48 -0600

    Add submission scripts

---
+++ src/gp/results/submit_mpi_16_openmp_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J gp_ddr
+#SBATCH -o gp_ddr_%j.txt
+#SBATCH -n 272
+#SBATCH -N 1
+#SBATCH -p normal
+#SBATCH -t 48:00:00
+#SBATCH --mail-user=damon@ices.utexas.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+env
+echo -n "Begin: "
+date
+export I_MPI_PIN_PROCESSOR_EXCLUDE_LIST=16,17,34,35,84,85,102,103,152,153,170,171,220,221,238,239
+export I_MPI_DEBUG=4
+export OMP_DISPLAY_ENV=TRUE
+ibrun -np 16 ./gp 1 queso_input_16.txt 5000
+echo -n "End: "
+date
