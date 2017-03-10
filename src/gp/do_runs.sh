5bf2859512c6b75657abb81def41ca9fc62b9163
Damon McDougall <damon@ices.utexas.edu>
2017-03-10 08:21:15 -0600

    Adding simple runner script

---
+++ src/gp/do_runs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+set -x
+
+./gp   1 queso_input_1.txt 20000
+./gp   2 queso_input_1.txt 20000
+./gp   4 queso_input_1.txt 20000
+./gp   8 queso_input_1.txt 20000
+./gp  16 queso_input_1.txt 20000
+./gp  32 queso_input_1.txt 20000
+./gp  64 queso_input_1.txt 20000
+./gp 128 queso_input_1.txt 20000
+./gp 256 queso_input_1.txt 20000
