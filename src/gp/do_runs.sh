d7ee7acb5dd6b15c93c8a81021e89f6e30d32016
Damon McDougall <damon.mcdougall@gmail.com>
2017-05-10 22:15:10 -0500

    Adding Xeon E52670 scripts and results

--- src/gp/do_runs.sh
+++ src/gp/do_runs.sh
@@ -3,12 +3,12 @@ set -eu
 set -o pipefail
 set -x
 
-./gp   1 queso_input_1.txt 20000
-./gp   2 queso_input_1.txt 20000
-./gp   4 queso_input_1.txt 20000
-./gp   8 queso_input_1.txt 20000
-./gp  16 queso_input_1.txt 20000
-./gp  32 queso_input_1.txt 20000
-./gp  64 queso_input_1.txt 20000
-./gp 128 queso_input_1.txt 20000
-./gp 256 queso_input_1.txt 20000
+./gp   1 queso_input_1.txt 40000
+./gp   2 queso_input_1.txt 40000
+./gp   4 queso_input_1.txt 40000
+./gp   8 queso_input_1.txt 40000
+./gp  16 queso_input_1.txt 40000
+./gp  32 queso_input_1.txt 40000
+./gp  64 queso_input_1.txt 40000
+./gp 128 queso_input_1.txt 40000
+./gp 256 queso_input_1.txt 40000
